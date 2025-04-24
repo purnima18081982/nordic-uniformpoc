b55d9867078da17440868807f1bb38c3598e1c98
Mehul <chaudhary.mehul@infosys.com>
2025-04-24 18:29:03 +0530

    added step 2 form

--- src/components/canvas/SignupStep/SignupStepTwoForm.tsx
+++ src/components/canvas/SignupStep/SignupStepTwoForm.tsx
@@ -1,95 +1,202 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
 import { ComponentProps } from '@uniformdev/canvas-next-rsc/component';
 
+type FormField = {
+  id: string;
+  name: string;
+  type: string;
+  label: string;
+  width: string;
+  placeholder?: string;
+  options?: Array<{ label: string; value: string }>;
+};
+
 type SignupStepTwoFormProps = {
-  steptwoinputs: {
-    inputs: Array<{
-      type: string;
-      id: string;
-      name: string;
+  label: string;
+  locationLabel: string;
+  placeholder: string;
+  locationOptionsJson: {
+    options: Array<{
+      value: string;
       label: string;
-      placeholder: string;
-      options: Array<{
-        value: string;
-        label: string;
-      }>;
     }>;
+  };
+  peopleWorkAtYourCompanyLabel: string;
+  peopleWorkAtYourCompanyOptions: {
+    options: Array<{
+      value: string;
+      label: string;
+    }>;
+  };
+  steptwoinputs: {
+    form: {
+      rows: Array<Array<FormField>>;
+    };
   };
 };
 
 type SignupStepTwoFormComponentProps = ComponentProps<SignupStepTwoFormProps>;
 
 const SignupStepTwoForm = (props: SignupStepTwoFormComponentProps) => {
-  const { steptwoinputs } = props;
+  const {
+    steptwoinputs,
+    label,
+    locationLabel,
+    locationOptionsJson,
+    placeholder,
+    peopleWorkAtYourCompanyLabel,
+    peopleWorkAtYourCompanyOptions,
+  } = props;
+
+  const [formData, setFormData] = useState({});
+
+  const handleInputChange = (key: string, value: string) => {
+    setFormData(prevState => ({ ...prevState, [key]: value }));
+  };
+
+  const handleSubmit = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    console.info('Form submitted:', formData);
+  };
 
   return (
     <>
-    <label>{uniformLabel}</label>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-3 px-8 py-4 lg:gap-6">
+        <div className="flex flex-col gap-3 lg:flex-row lg:gap-6">
+          <div className="flex w-full flex-col gap-1 lg:w-1/2">
+            <label className="text-sm">{label}</label>
+            <input
+              id={'whatCompanyDoYouWorkFor'}
+              placeholder={placeholder}
+              className="border border-gray-300 px-4 py-2"
+              onChange={e => handleInputChange('whatCompanyDoYouWorkFor', e.target.value)}
+            />
+          </div>
+          <div className="hidden w-full gap-1 lg:block lg:w-1/2"></div>
+        </div>
+        <div className="flex flex-col gap-3 lg:flex-row lg:gap-6">
+          <div className="flex w-full flex-col gap-1 lg:w-1/2">
+            <label className="text-sm">{locationLabel}</label>
+            <select
+              className="border border-gray-300 px-4 py-2"
+              onChange={e => handleInputChange('location', e.target.value)}
+            >
+              {locationOptionsJson?.options.map(option => (
+                <option key={option.value} value={option.value}>
+                  {option.label}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="flex w-full flex-col gap-1 lg:w-1/2">
+            <label className="text-sm">{peopleWorkAtYourCompanyLabel}</label>
+            <select
+              className="border border-gray-300 px-4 py-2"
+              onChange={e => handleInputChange('numberOfPeopleWorkAtYourCompany', e.target.value)}
+            >
+              {peopleWorkAtYourCompanyOptions?.options.map(option => (
+                <option key={option.value} value={option.value}>
+                  {option.label}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
 
-        <input type="text" /><input type="text" /><input type="text" />
-
-
-      {steptwoinputs?.inputs.map(inputs => {
-        return (
-          <>
-            <div className="flex flex-1 p-8" key={inputs.id}>
-              {inputs.type === 'text' && (
-                <div className="flex w-full flex-col gap-2 lg:w-1/{noOFInputs}">
-                  <label>{inputs.label}</label>
-                  <input id={inputs.id} placeholder={inputs.placeholder} className="border border-gray-300 px-4 py-2" />
-                </div>
-              )}
-
-              {inputs.type === 'select' && (
-                <div className="flex w-full flex-col gap-2 lg:w-1/2">
-                  <label>{inputs.label}</label>
-                  <select id={inputs.id} className="border border-gray-300 px-4 py-2">
-                    {inputs.options.map(option => (
-                      <option key={option.value} value={option.value}>
-                        {option.label}
-                      </option>
-                    ))}
-                  </select>
-                </div>
-              )}
-            </div>
-          </>
-        );
-      })}
+        <div className="flex w-full items-center justify-between gap-2">
+          <button className="w-1/12 bg-blue-400 px-4 py-2 text-white" type="button">
+            Cancel
+          </button>
+          <button className="w-1/12 bg-blue-400 px-4 py-2 text-white" type="submit">
+            Submit
+          </button>
+        </div>
+      </form>
     </>
   );
 };
 
 export default SignupStepTwoForm;
 
+// {
+//   "form": {
+//     "rows": [
+//       [
+//         {
+//           "id": "whatCompanyDoYouWorkFor",
+//           "name": "whatCompanyDoYouWorkFor",
+//           "type": "text",
+//           "label": "What Company Do You Work For",
+//           "width": "w-full lg:w-1/2",
+//           "placeholder": "Please Enter Your Company Name"
+//         },
+//         {
+//           "id": "whatCompanyDoYouWorkFor",
+//           "name": "whatCompanyDoYouWorkFor",
+//           "type": "text",
+//           "label": "What Company Do You Work For",
+//           "width": "w-full lg:w-1/2",
+//           "placeholder": "Please Enter Your Company Name"
+//         }
+//       ],
+//       [
+//         {
+//           "id": "location",
+//           "name": "location",
+//           "type": "select",
+//           "label": "Location",
+//           "width": "w-full lg:w-1/2",
+//           "options": [
+//             {
+//               "label": "Select Location",
+//               "value": ""
+//             },
+//             {
+//               "label": "Option 1",
+//               "value": "option_1"
+//             }
+//           ],
+//           "placeholder": "Please Select your location"
+//         }
+//       ]
+//     ]
+//   }
+// }
 
-{
-    form: {
-        row1: {
-            noofInputs: 3,
-            children: [
-                {
-                    "type": "text",
-                    "id": "whatCompanyDoYouWorkFor",
-                    "name": "whatCompanyDoYouWorkFor",
-                    "label": "What Company Do You Work For",
-                    "placeholder": "Please Enter Your Company Name"
-                  },
-                  {
-                    "type": "text",
-                    "id": "whatCompanyDoYouWorkFor",
-                    "name": "whatCompanyDoYouWorkFor",
-                    "label": "What Company Do You Work For",
-                    "placeholder": "Please Enter Your Company Name"
-                  },
-                  {
-                    "type": "text",
-                    "id": "whatCompanyDoYouWorkFor",
-                    "name": "whatCompanyDoYouWorkFor",
-                    "label": "What Company Do You Work For",
-                    "placeholder": "Please Enter Your Company Name"
-                  },
-            ]
-        }
-    }
-}
+// {steptwoinputs?.form.rows.map((row, rowIndex) => {
+//   return (
+//     <div className="flex gap-2" key={rowIndex}>
+//       {row.map(input => {
+//         return (
+//           <div className={`${input.width} flex  flex-col gap-2 `} key={input.id}>
+//             <label className="text-sm">{input.label}</label>
+//             {input.type === 'text' && (
+//               <input
+//                 id={input.id}
+//                 placeholder={input.placeholder}
+//                 className="border border-gray-300 px-4 py-2"
+//                 onChange={e => handleInputChange(input.id, e.target.value)}
+//               />
+//             )}
+//             {input.type === 'select' && (
+//               <select
+//                 id={input.id}
+//                 className="border border-gray-300 px-4 py-2"
+//                 onChange={e => handleInputChange(input.id, e.target.value)}
+//               >
+//                 {input.options?.map(option => (
+//                   <option key={option.value} value={option.value}>
+//                     {option.label}
+//                   </option>
+//                 ))}
+//               </select>
+//             )}
+//             {input.type === 'checkbox' && <input type="checkbox" />}
+//           </div>
+//         );
+//       })}
+//     </div>
+//   );
+// })}
