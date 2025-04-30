513c5c0603177705145351ce3fcae6b51ed967c1
dhiraj-kadam_infosys <dhiraj.kadam@infosys.com>
2025-04-30 15:08:57 +0530

    checkboxupdate

--- src/components/canvas/SignupStep4/SignupStep4.tsx
+++ src/components/canvas/SignupStep4/SignupStep4.tsx
@@ -1,18 +1,16 @@
 'use client';
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { UniformRichText, UniformText } from '@uniformdev/canvas-next-rsc/component';
 import BaseImage from '@/components/ui/Image';
 import { SignupStep4Props } from '.';
 
 export const SignupStep4: FC<SignupStep4Props> = ({ component, context }) => {
   // Access the emails array from the emailData object
+  const [checkedItems, setCheckedItems] = useState<{ id: number }[]>([]);
+  //const [allCheck, setAllCheck] = useState<boolean>();
+
   const icon = (component?.parameters?.icon?.value as { fields: { url: { value: string } } }[] | undefined)?.[0]?.fields
     ?.url?.value;
-  console.info(component?.parameters?.emailtype?.value, 'DES');
-  //   const emailTypes =
-  //     (component?.parameters?.emailtype?.value as
-  //       | { id: number; title: string; shortdescription: string }[]
-  //       | undefined) || [];
   const emailTypes =
     (
       component?.parameters?.emailtype?.value as
@@ -27,7 +25,21 @@ export const SignupStep4: FC<SignupStep4Props> = ({ component, context }) => {
           }
         | undefined
     )?.emailtype || [];
-  console.info(emailTypes[0]?.title, 'Email Types');
+  const toggleItem = (subtype: { id: number; title?: string; shortdescription?: string }) => {
+    if (checkedItems.includes(subtype)) {
+      setCheckedItems(checkedItems.filter(i => i !== subtype));
+    } else {
+      setCheckedItems([...checkedItems, subtype]);
+    }
+  };
+  const toggleAll = (subtype: { id: number }[]) => {
+    if (checkedItems.length === subtype.length) {
+      setCheckedItems([]);
+    } else {
+      setCheckedItems([...subtype]);
+    }
+  };
+
   return (
     <>
       <div className="w-full py-10">
@@ -41,7 +53,7 @@ export const SignupStep4: FC<SignupStep4Props> = ({ component, context }) => {
       </div>
       <div className="w-full py-10">
         <UniformText
-          className="text-#000000 font-bold font-sans text-2xl"
+          className="text-#000000 font-sans text-2xl font-bold"
           context={context}
           component={component}
           parameterId="title"
@@ -66,7 +78,7 @@ export const SignupStep4: FC<SignupStep4Props> = ({ component, context }) => {
         </div>
         <br />
         <UniformText
-          className="text-#000000 font-bold font-sans text-2xl"
+          className="text-#000000 font-sans text-2xl font-bold"
           context={context}
           component={component}
           parameterId="title1"
@@ -80,33 +92,42 @@ export const SignupStep4: FC<SignupStep4Props> = ({ component, context }) => {
           parameterId="description1"
           as="p"
         />
-        {emailTypes.map(data => (
-          <ul key={data?.id} className="border border-[#D9E1E2] mb-5 p-10">
-            <label htmlFor="item1" className="text-gray flex items-center">
-              <input
-                type="checkbox"
-                id="item1"
-                className="mr-2 h-4 w-4 text-blue-600 border-gray-300 rounded focus:ring-blue-500"
-              />
-              <div className="pl-1 pr-1 pt-0">
-                <BaseImage src={data?.icon || '/default-icon.png'} alt={'title'} width={40} height={40} />
-              </div>
-              {data?.title}
-            </label>
-            <p className="text-gray-300 pl-7"> {data?.shortdescription}</p>
-            {data?.subtype?.map((subtype, index) => (
-              <li key={index} className="items-center pl-10">
+        {emailTypes.map(data => {
+          return (
+            <ul key={data?.id} className="mb-5 border border-[#D9E1E2] p-10">
+              <label htmlFor="item1" className="text-gray flex items-center">
                 <input
                   type="checkbox"
-                  id="item1"
-                  className="mr-2 h-4 w-4 text-blue-600 border-gray-300 rounded focus:ring-blue-500"
+                  id={data?.id?.toString()}
+                  name={data?.id?.toString()}
+                  checked={checkedItems.length === data?.subtype?.length ? true : false}
+                  onChange={() => toggleAll(data?.subtype || [])}
+                  className="mr-2 size-4 rounded border-gray-300 text-blue-600 focus:ring-blue-500"
                 />
-                {subtype?.title}
-                <p className="text-gray-300 pl-7"> {subtype?.shortdescription}</p>
-              </li>
-            ))}
-          </ul>
-        ))}
+                <div className="px-1 pt-0">
+                  <BaseImage src={data?.icon || '/default-icon.png'} alt={'title'} width={40} height={40} />
+                </div>
+                {data?.title}
+              </label>
+              <p className="pl-7 text-gray-300"> {data?.shortdescription}</p>
+              {data?.subtype?.map(subtype => {
+                return (
+                  <li key={subtype?.id} className="items-center pl-10">
+                    <input
+                      type="checkbox"
+                      id={subtype?.id?.toString()}
+                      name={subtype?.id?.toString()}
+                      checked={checkedItems.includes(subtype)}
+                      onChange={() => toggleItem(subtype)}
+                      className="mr-2 size-4 rounded border-gray-300 text-blue-600 focus:ring-blue-500"
+                    />
+                    {subtype?.title}
+                  </li>
+                );
+              })}
+            </ul>
+          );
+        })}
       </div>
     </>
   );
